b4b153199cdd370550a4ea36ee03dc49ce394588
Vladyslav Kuidin <vkuydi@gmail.com>
2022-06-04 21:54:51 +0300

    feat: add files with data

---
+++ src/js/data/statistics-table-data.js
@@ -0,0 +1,29 @@
+const columns = [
+  {
+    name: 'full_name',
+    title: 'Name',
+    directionImgPath: '/images/down-arrow.png',
+  },
+  {
+    name: 'course',
+    title: 'Speciality',
+    directionImgPath: '/images/down-arrow.png',
+  },
+  {
+    name: 'age',
+    title: 'Age',
+    directionImgPath: '/images/down-arrow.png',
+  },
+  {
+    name: 'gender',
+    title: 'Gender',
+    directionImgPath: '/images/down-arrow.png',
+  },
+  {
+    name: 'country',
+    title: 'Country',
+    directionImgPath: '/images/down-arrow.png',
+  },
+]
+
+export default columns
