cc82a72c214447c7ba3f8095e0dc87ba969e8295
Vladyslav Kuidin <vkuydi@gmail.com>
2022-06-05 16:05:48 +0300

    feat: temp comm

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
-import '../styles/main.scss';
+import '../styles/main.scss'
+import { fillUserList } from './user-displaying/user-index'
+import { formattedUsers } from './data/users-data'
+import fillStatisticsTable from './table/statistics-table'
+
+fillUserList('.teachers-list', formattedUsers)
+fillStatisticsTable(formattedUsers)
---
+++ src/js/user-displaying/user-index.js
@@ -0,0 +1,46 @@
+/* eslint-disable quote-props */
+import { createElement } from '../helpers/helper'
+
+export function createUser({ user, job = true }) {
+  const teacherItem = createElement('li', {
+    className: 'teacher-item teachers-list__item',
+    'data-id': user.id,
+  })
+  const teacherAvatar = createElement('img', {
+    className: 'teacher-item__img',
+    srcset: user.picture_large,
+  })
+  const teacherName = createElement('div', {
+    className: 'teacher-item__name',
+    textContent: user.full_name,
+  })
+  const teacherLocation = createElement('div', {
+    className: 'teacher-item__location',
+    textContent: user.country,
+  })
+
+  teacherItem.appendChild(teacherAvatar)
+  teacherItem.appendChild(teacherName)
+  if (job) {
+    teacherItem.appendChild(createElement('div', {
+      className: 'teacher-item__job',
+      textContent: user.course,
+    }))
+  }
+  teacherItem.appendChild(teacherLocation)
+
+  if (user.favorite) {
+    teacherItem.appendChild(createElement('div', {
+      className: 'teacher-item__favorite',
+    }))
+  }
+
+  return teacherItem
+}
+
+export function fillUserList(listSelector, users) {
+  const list = document.querySelector(listSelector)
+  users.forEach(user => list.appendChild(createUser({
+    user,
+  })))
+}
