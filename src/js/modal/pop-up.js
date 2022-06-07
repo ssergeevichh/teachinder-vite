1eaa10a5f03c70132118beb4b866035bbc529680
Vladyslav Kuidin <vkuydi@gmail.com>
2022-06-07 17:12:28 +0300

    feat: create universal modal

---
+++ src/js/modal/pop-up.js
@@ -0,0 +1,61 @@
+const defaultProps = {
+  clickOutside: true,
+  fullScreen: false,
+}
+
+export function openModal(selector, options = defaultProps) {
+  const modalLayer = document.createElement('div')
+  modalLayer.className = 'modal-overlay'
+  setTimeout(() => {
+    modalLayer.style.visibility = 'visible'
+    modalLayer.style.opacity = 1
+  }, 100)
+
+  function createCloseEl() {
+    const closeBtn = document.createElement('div')
+    const span1 = document.createElement('span')
+    const span2 = document.createElement('span')
+
+    closeBtn.className = 'close-btn'
+
+    span1.classList.add('close__line')
+    span2.classList.add('close__line')
+
+    closeBtn.append(span1)
+    closeBtn.append(span2)
+
+    return closeBtn
+  }
+  const modalWindow = document.querySelector(selector)
+  const headerBlock = modalWindow.querySelector('.modal-header')
+  const closeBtn = createCloseEl()
+
+  document.body.prepend(modalLayer)
+  modalLayer.append(modalWindow)
+
+  modalWindow.classList.add('modal-block')
+  headerBlock.append(closeBtn)
+
+  if (options.fullScreen) {
+    modalWindow.classList.add('full-screen')
+  }
+
+  function closeModal() {
+    setTimeout(() => {
+      modalLayer.remove()
+      closeBtn.remove()
+    }, 800)
+
+    modalLayer.style.visibility = 'hidden'
+    modalLayer.style.opacity = 0
+  }
+
+  document.addEventListener('click', ({ target }) => {
+    if ((!options.clickOutside && target) === closeBtn || target.className === 'close__line') {
+      closeModal()
+    }
+    else if ((options.clickOutside && target.classList.contains('modal-overlay')) || target === closeBtn || target === closeBtn.firstChild) {
+      closeModal()
+    }
+  })
+}
