497a6c8c1b835dffc533ad14686c1c475b1e60ac
Vladyslav Kuidin <vkuydi@gmail.com>
2022-06-10 23:29:28 +0300

    feat: add possibility to clear favorite users

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import '../styles/main.scss'
 import { initPupilsStatisticsTable, initTeacherStatisticsTable } from './user/table'
+import { initQuantityOfFavorites } from '@/js/user/favorites-quantity-inner'
 import { initUserList } from '@/js/user/user-list'
 
+initQuantityOfFavorites()
 initUserList()
 initTeacherStatisticsTable()
 initPupilsStatisticsTable()
