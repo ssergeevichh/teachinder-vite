fd3d331afa0f4e7e80e64beb0738adf9e60fcb8b
Vladyslav Kuidin <vkuydi@gmail.com>
2022-06-12 20:14:24 +0300

    feat: add getAge function

--- src/js/helpers/helper.js
+++ src/js/helpers/helper.js
@@ -14,3 +14,14 @@ export function createElement(tag, props) {
 export function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
+
+export function getAge(date) {
+  const today = new Date()
+  const birthDate = new Date(date)
+  let age = today.getFullYear() - birthDate.getFullYear()
+  const m = today.getMonth() - birthDate.getMonth()
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--
+  }
+  return age
+}
